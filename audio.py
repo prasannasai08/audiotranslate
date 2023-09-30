c650b77ae5aad173c3b081656d37a144f64aea18
prasannasai08 <81038590+prasannasai08@users.noreply.github.com>
2023-09-30 21:56:08 +0530

    Add files via upload

---
+++ audio.py
@@ -0,0 +1,16 @@
+import googletrans
+import speech_recognition
+import gtts
+import os
+from playsound import playsound
+audio_file_path = r'C:\\Users\\hhh\\Downloads\\harvard.wav'
+recognizer = speech_recognition.Recognizer()
+with speech_recognition.AudioFile(audio_file_path) as source:
+    voice = recognizer.listen(source)
+    text = recognizer.recognize_google(voice, language="en")
+translator = googletrans.Translator()
+translation = translator.translate(text, dest="hi")
+converted_audio = gtts.gTTS(translation.text, lang="hi")
+converted_audio.save('captured_voice.mp3')
+playsound('captured_voice.mp3')
+os.remove('captured_voice.mp3')
