5d9a7e3e64da98e1b6b5697a4e5fedb1a46c9456
prasannasai08 <81038590+prasannasai08@users.noreply.github.com>
2023-09-30 22:32:48 +0530

    audio.py

--- audio.py
+++ audio.py
@@ -3,7 +3,7 @@ import speech_recognition
 import gtts
 import os
 from playsound import playsound
-audio_file_path = r'C:\\Users\\hhh\\Downloads\\harvard.wav'
+audio_file_path = r'C:\\Users\\hhh\\Downloads\\SundarPichai.mp3'
 recognizer = speech_recognition.Recognizer()
 with speech_recognition.AudioFile(audio_file_path) as source:
     voice = recognizer.listen(source)
@@ -11,6 +11,11 @@ with speech_recognition.AudioFile(audio_file_path) as source:
 translator = googletrans.Translator()
 translation = translator.translate(text, dest="hi")
 converted_audio = gtts.gTTS(translation.text, lang="hi")
+converted_audio.save('captured_voice.mp3')
+playsound('captured_voice.mp3')
+os.remove('captured_voice.mp3')
+translation = translator.translate(text, dest="te")
+converted_audio = gtts.gTTS(translation.text, lang="te")
 converted_audio.save('captured_voice.mp3')
 playsound('captured_voice.mp3')
 os.remove('captured_voice.mp3')
